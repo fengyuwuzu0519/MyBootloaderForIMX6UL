a8078984936e132662833f201b97fb1d8e137645
100ask <yangf0519@163.com>
2021-04-07 22:58:23 +0800

    feature-led: 根据硬件LED管脚,修改实现LED持续闪烁功能
        本实验硬件的LED管脚为GPIO3_IO04

--- led.c
+++ led.c
@@ -1,10 +1,10 @@
 
 #include "led.h"
-
-static volatile unsigned int *CCM_CCGR1                              ;
-static volatile unsigned int *IOMUXC_SNVS_SW_MUX_CTL_PAD_SNVS_TAMPER3;
-static volatile unsigned int *GPIO5_GDIR                             ;
-static volatile unsigned int *GPIO5_DR                               ;
+//gpio3-4
+static volatile unsigned int *CCM_CCGR2                              ;
+static volatile unsigned int *IOMUXC_SW_MUX_CTL_PAD_LCD_RESET;
+static volatile unsigned int *GPIO3_GDIR                             ;
+static volatile unsigned int *GPIO3_DR                               ;
 
 /**********************************************************************
  * 函数名称： led_init
@@ -20,37 +20,35 @@ void led_init(void)
 {
 	unsigned int val;
 
-	CCM_CCGR1                               = (volatile unsigned int *)(0x20C406C);
-	IOMUXC_SNVS_SW_MUX_CTL_PAD_SNVS_TAMPER3 = (volatile unsigned int *)(0x2290014);
-	GPIO5_GDIR                              = (volatile unsigned int *)(0x020AC000 + 0x4);
-	GPIO5_DR                                = (volatile unsigned int *)(0x020AC000);
+	CCM_CCGR2                               = (volatile unsigned int *)(0x20C406C);
+	IOMUXC_SW_MUX_CTL_PAD_LCD_RESET			= (volatile unsigned int *)(0x20e0114);
+	GPIO3_GDIR                              = (volatile unsigned int *)(0x020A4000 + 0x4);
+	GPIO3_DR                                = (volatile unsigned int *)(0x020A4000);
 
-	/* GPIO5_IO03 */
-	/* a. 使能GPIO5
-	 * set CCM to enable GPIO5
-	 * CCM_CCGR1[CG15] 0x20C406C
-	 * bit[31:30] = 0b11
+	/* GPIO3_IO04 */
+	/* a. 使能GPIO3
+	 * set CCM to enable GPIO3
+	 * bit[27:26] = 0b11
 	 */
-	*CCM_CCGR1 |= (3<<30);
+	*CCM_CCGR2 |= (3<<26);
 
-	/* b. 设置GPIO5_IO03用于GPIO
-	 * set IOMUXC_SNVS_SW_MUX_CTL_PAD_SNVS_TAMPER3
-	 *      to configure GPIO5_IO03 as GPIO
-	 * IOMUXC_SNVS_SW_MUX_CTL_PAD_SNVS_TAMPER3  0x2290014
+	/* b. 设置GPIO3_IO04用于GPIO
+	 * set IOMUXC_SW_MUX_CTL_PAD_LCD_RESET
+	 *      to configure GPIO3_IO04 as GPIO
 	 * bit[3:0] = 0b0101 alt5
 	 */
-	val = *IOMUXC_SNVS_SW_MUX_CTL_PAD_SNVS_TAMPER3;
+	val = *IOMUXC_SW_MUX_CTL_PAD_LCD_RESET;
 	val &= ~(0xf);
 	val |= (5);
-	*IOMUXC_SNVS_SW_MUX_CTL_PAD_SNVS_TAMPER3 = val;
+	*IOMUXC_SW_MUX_CTL_PAD_LCD_RESET = val;
 
 
-	/* c. 设置GPIO5_IO03作为output引脚
-	 * set GPIO5_GDIR to configure GPIO5_IO03 as output
-	 * GPIO5_GDIR  0x020AC000 + 0x4
-	 * bit[3] = 0b1
+	/* c. 设置GPIO3_IO04作为output引脚
+	 * set GPIO3_GDIR to configure GPIO3_IO04 as output
+	 * GPIO3_GDIR  0x020AC000 + 0x4
+	 * bit[4] = 0b1
 	 */
-	*GPIO5_GDIR |= (1<<3);
+	*GPIO3_GDIR |= (1<<4);
 
 }
 
@@ -69,20 +67,18 @@ void led_ctl(int on)
 {
 	if (on) /* on: output 0*/
 	{
-		/* d. 设置GPIO5_DR输出低电平
-		 * set GPIO5_DR to configure GPIO5_IO03 output 0
-		 * GPIO5_DR 0x020AC000 + 0
-		 * bit[3] = 0b0
+		/* d. 设置GPIO3_DR输出低电平
+		 * set GPIO3_DR to configure GPIO3_IO04 output 0
+		 * bit[4] = 0b0
 		 */
-		*GPIO5_DR &= ~(1<<3);
+		*GPIO3_DR &= ~(1<<4);
 	}
 	else  /* off: output 1*/
 	{
 		/* e. 设置GPIO5_IO3输出高电平
-		 * set GPIO5_DR to configure GPIO5_IO03 output 1
-		 * GPIO5_DR 0x020AC000 + 0
-		 * bit[3] = 0b1
+		 * set GPIO3_DR to configure GPIO3_IO04 output 1
+		 * bit[4] = 0b1
 		 */
-		*GPIO5_DR |= (1<<3);
+		*GPIO3_DR |= (1<<4);
 	}
 }
