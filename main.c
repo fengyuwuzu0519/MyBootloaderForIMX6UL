1282477de2f9494dcb787483cb46d89d8e4fc25c
100ask <yangf0519@163.com>
2021-04-10 15:35:15 +0800

    feature-cpu: 设置SVC模式、打开icache、禁止中断/MMU

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "uart.h"
 #include "my_printf.h"
 #include "clk.h"
+#include "io.h"
 
 void delay(volatile unsigned int d)
 {
@@ -10,16 +11,18 @@ void delay(volatile unsigned int d)
 
 int  main()
 {
+	u32 cnt=0;
 	led_init();
 	uart_init();
 
 	while(1)
 	{
+		cnt++;
 		led_ctl(1);
 		delay(1000000);
 		led_ctl(0);
 		delay(1000000);
-		printf("clk=%d\r\n", mxc_get_clock(MXC_ARM_CLK));
+		printf("cnt=%d clk=%d\r\n", cnt, mxc_get_clock(MXC_ARM_CLK));
 	}
 
 	return 0;
